e02370cc65f706645646cbc9ce622395a2214ce5
ColinH58 <choolwerf@gmail.com>
2022-11-19 14:03:31 -0600

    finished

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -6,8 +6,8 @@ const Profile = () => {
       <h1>This page is unfinished!</h1>
       <h2>Please don't hold that against me</h2>
       <h3>I would finish it but I don't think this project will last very long</h3>
-      <h4>It's hosted on Heroku and the free version of that is going away soon...</h4>
-      <h5>But also I had a family emergency when we started this project so I wasn't there to help my team with it</h5>
+      <h4>It's back end is hosted on Heroku and the free version of that is going away soon...</h4>
+      <h5>But also I had a family emergency when we started this project so I wasn't there to help my team finish it</h5>
       <h6>Have a nice day! 🙂</h6>
     </div>
   );
