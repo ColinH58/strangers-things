0406f309b2125c4bc8a82740f7140bae2e1f043c
ColinH58 <choolwerf@gmail.com>
2022-11-19 13:50:41 -0600

    ready to run the build

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>WIP Profile Page!</div>;
+  return (
+    <div className="Profile">
+      <h1>This page is unfinished!</h1>
+      <h2>Please don't hold that against me</h2>
+      <h3>I would finish it but I don't think this project will last very long</h3>
+      <h4>It's hosted on Heroku and the free version of that is going away soon...</h4>
+      <h5>But also I had a family emergency when we started this project so I wasn't there to help my team with it</h5>
+      <h6>Have a nice day! 🙂</h6>
+    </div>
+  );
 };
 
 export default Profile;
